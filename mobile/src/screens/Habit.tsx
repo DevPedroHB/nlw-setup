d39d2c07346a2ab4d00d25b69daa59b58a9ae523
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-21 11:24:05 -0300

    Lesson 05 The next level

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,20 +1,91 @@
 import { useRoute } from "@react-navigation/native";
+import clsx from "clsx";
 import dayjs from "dayjs";
-import { ScrollView, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { HabitEmpty } from "../components/HabitEmpty";
+import { Loading } from "../components/Loading";
 import { ProgressBar } from "../components/Progress.Bar";
+import { api } from "../lib/axios";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 interface Params {
   date: string;
 }
 
+interface DayInfo {
+  possibleHabits: {
+    id: string;
+    title: string;
+    created_at: string;
+  }[];
+  completedHabits: string[];
+}
+
 export function Habit() {
+  const [loading, setLoading] = useState(true);
+  const [dayInfo, setDayInfo] = useState<DayInfo | null>(null);
+  const [completedHabits, setCompletedHabits] = useState<string[]>([]);
   const route = useRoute();
   const { date } = route.params as Params;
   const parsedDate = dayjs(date);
+  const isDateInPassed = parsedDate.endOf("day").isBefore(new Date());
   const dayOfWeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
+  const habitsProgress = dayInfo?.possibleHabits.length
+    ? generateProgressPercentage(
+        dayInfo.possibleHabits.length,
+        completedHabits.length
+      )
+    : 0;
+
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get("day", {
+        params: {
+          date,
+        },
+      });
+
+      setDayInfo(response.data);
+      setCompletedHabits(response.data.completedHabits);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Ops",
+        "Não foi possível carregar as informações dos hábitos"
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleToggleHabit(habitId: string) {
+    try {
+      await api.patch(`habits/${habitId}/toggle`);
+
+      if (completedHabits.includes(habitId)) {
+        setCompletedHabits((prev) => prev.filter((id) => id !== habitId));
+      } else {
+        setCompletedHabits((prev) => [...prev, habitId]);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ops", "Não foi possível atualizar o status do hábito.");
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -29,11 +100,27 @@ export function Habit() {
         <Text className="text-white font-extrabold text-3xl">
           {dayAndMonth}
         </Text>
-        <ProgressBar progress={30} />
-        <View className="mt-6">
-          <Checkbox title="Beber 2L de água" checked={false} />
-          <Checkbox title="Caminhar" checked />
+        <ProgressBar progress={habitsProgress} />
+        <View className={clsx("mt-6", { ["opacity-50"]: isDateInPassed })}>
+          {dayInfo?.possibleHabits ? (
+            dayInfo?.possibleHabits.map((habit) => (
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPassed}
+                onPress={() => handleToggleHabit(habit.id)}
+              />
+            ))
+          ) : (
+            <HabitEmpty />
+          )}
         </View>
+        {isDateInPassed && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,11 @@ const app = Fastify();
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({
-  port: 3333,
-});
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then((url) => {
+    console.log(`HTTP server running on ${url}`);
+  });
